618e4fab4d6c3559ec41026922c59063ef45745b
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-05 10:55:17 +0300

    Created the Navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
-
+import  { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 
 const App = () => {
   return (
-    <div className="App container mx-auto">
-      
+    <div className="App container px-8 h-screen mx-auto md:w-3/4 lg:w-1/2 md:px-0">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
-        <div className="Header">
-
+        <div className="Header w-full a:cursor-pointer sticky top-0 z-30 bg-white py-6">
+            <div className="flex items-center justify-between">
+                <Link to="/" className="cursor-pointer">
+                    <h1 className="font-bold text-3xl text-pink-500">
+                        The Ninja Blog
+                    </h1>
+                </Link>
+                <div className="flex space-x-4 items-center pt-2">
+                    <Link to="/" >
+                        <button className="p-4 hover:text-white hover:bg-pink-500 hover:rounded-md">
+                            Home
+                        </button>
+                    </Link>
+                    <Link to="/">
+                        <button className="text-white bg-pink-500 p-4 rounded-md hover:bg-white hover:text-pink-500">
+                            New Blog
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
