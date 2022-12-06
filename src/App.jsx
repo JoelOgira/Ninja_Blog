11be8b6c421f87d59fb4f785a1d3f5e30a6d1549
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-06 22:37:44 +0300

    Finished up the create nw blog funcionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import  { Routes, Route } from "react-router-dom";
+import  { Routes, Route, useNavigate } from "react-router-dom";
 import api from "./api/blogs"
+import {format} from "date-fns"
 import BlogPage from "./Components/BlogPage";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
@@ -9,25 +10,55 @@ import New from "./Components/New";
 const App = () => {
 
   const [blogs, setBlogs] = useState([]);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [body, setBody] = useState('');
 
-    useEffect(() => {
-        const getBlogs = async () => {
-            try {
-                const response = await api.get('/blogs');
-                setBlogs(response.data);
-            } catch (error) {
-                console.log(error.message);   
-            }
-        }
-        getBlogs();
-    }, []);
+  const history = useNavigate();
+
+  useEffect(() => {
+      const getBlogs = async () => {
+          try {
+              const response = await api.get('/blogs');
+              setBlogs(response.data);
+          } catch (error) {
+              console.log(error.message);   
+          }
+      }
+      getBlogs();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = blogs.length ? blogs[blogs.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM yy, yyy pp');
+    const newBlog = {id, author, datetime, title, body}
+    try {
+      const response = await api.post('/blogs', newBlog);
+      setBlogs([...blogs, response.data]);
+      setTitle('');
+      setAuthor('');
+      setBody('');
+      history('/');
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <div className="App container px-8 h-screen mx-auto md:w-3/4 lg:w-1/2 md:px-0 bg-gray-200">
       <Header />
       <Routes>
         <Route path="/" element={<Home blogs={blogs} />} />
-        <Route path="/blog" element={<New />} />
+        <Route path="/blog" element={<New 
+          title={title}
+          setTitle={setTitle}
+          author={author}
+          setAuthor={setAuthor}
+          body={body}
+          setBody={setBody}
+          handleSubmit={handleSubmit}  
+        />} />
         <Route path="/blog/:id" element={<BlogPage blogs={blogs} />} />
       </Routes>
     </div>
--- src/Components/New.jsx
+++ src/Components/New.jsx
@@ -1,6 +1,35 @@
-const New = () => {
+const New = ({ 
+    author, setAuthor, title, setTitle, body, setBody, handleSubmit
+ }) => {
     return ( 
-        <div className="New">
+        <div classNameName="New">
+        <h1 className="text-center text-4xl underline font-semibold mb-3">
+            Create Blog
+        </h1>
+        <form onSubmit={handleSubmit}>
+            <div className="mb-6">
+                <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 ">Blog Title</label>
+                <input type="text" id="title" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:border-gray-600" placeholder="Title" required
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                />
+            </div>
+            <div className="mb-6">
+                <label htmlFor="author" className="block mb-2 text-sm font-medium text-gray-900 ">Author</label>
+                <input type="text" id="author" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-500 focus:border-pink-500 block w-full p-2.5 dark:border-gray-600" required
+                value={author}
+                onChange={e => setAuthor(e.target.value)}
+                />
+            </div>
+            <div className="mb-6">                
+                <label for="body" class="block mb-2 text-sm font-medium text-gray-900 ">Blog</label>
+                <textarea id="body" rows="12" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                value={body}
+                onChange={e => setBody(e.target.value)}
+                ></textarea>
+            </div>
+            <button type="submit" className="cursor-pointer text-white bg-pink-500 hover:bg-pink-900 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add Blog</button>
+        </form>
 
         </div>
      );
