7d74f5cbe616dc35dba652f012db67c985b887a1
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-06 18:40:44 +0300

    Added useEffect Hook to fecth data from database

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-
+import { useState, useEffect } from "react";
 import  { Routes, Route } from "react-router-dom";
+import api from "./api/blogs"
+import BlogPage from "./Components/BlogPage";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import New from "./Components/New";
 
 const App = () => {
+
+  const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        const getBlogs = async () => {
+            try {
+                const response = await api.get('/blogs');
+                setBlogs(response.data);
+            } catch (error) {
+                console.log(error.message);   
+            }
+        }
+        getBlogs();
+    }, []);
+
   return (
     <div className="App container px-8 h-screen mx-auto md:w-3/4 lg:w-1/2 md:px-0 bg-gray-200">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Home />} />
+        <Route path="/" element={<Home blogs={blogs} />} />
+        <Route path="/blog" element={<New />} />
+        <Route path="/blog/:id" element={<BlogPage blogs={blogs} />} />
       </Routes>
     </div>
   )
