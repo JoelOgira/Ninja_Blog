950f63ed859fc02d0d9798a8b31888972a26bf8e
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-07 18:09:37 +0300

    Added the search component in the header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,26 @@
 import { Link } from "react-router-dom";
+import Search from "./Search.";
 
-const Header = () => {
+const Header = ({ search, setSearch }) => {
     return (
         <div className="Header w-full a:cursor-pointer sticky top-0 z-30 bg-gray-200 py-6 mb-6">
             <div className="flex items-center justify-between">
-                <Link to="/" className="cursor-pointer">
+                <Link to="/" className="cursor-pointer pt-3">
                     <h1 className="font-semibold text-3xl text-pink-500">
                         The Ninja Blog
                     </h1>
                 </Link>
+                <div className="pt-3">
+                    <Search search={search} setSearch={setSearch} />
+                </div>
                 <div className="flex space-x-4 items-center pt-2">
                     <Link to="/" >
-                        <button className="p-4 hover:text-white hover:bg-pink-500 hover:rounded-md">
+                        <button className="p-3 border-solid border-2 border-pink-500 hover:text-white hover:bg-pink-500 rounded-md">
                             Home
                         </button>
                     </Link>
                     <Link to="/blog">
-                        <button className="text-white bg-pink-500 p-4 rounded-md hover:bg-transparent hover:text-pink-500">
+                        <button className="text-white border-solid border-2 border-pink-500 bg-pink-500 p-3 rounded-md hover:bg-transparent hover:text-black">
                             New Blog
                         </button>
                     </Link>
