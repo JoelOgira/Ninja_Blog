659f1b3a6d1c943fe20688429ddf6cd05471d21f
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-07 11:29:19 +0300

    Changed styling in the Create new blog

--- src/Components/New.jsx
+++ src/Components/New.jsx
@@ -22,8 +22,8 @@ const New = ({
                 />
             </div>
             <div className="mb-6">                
-                <label for="body" class="block mb-2 text-sm font-medium text-gray-900 ">Blog</label>
-                <textarea id="body" rows="12" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                <label htmlFor="body" class="block mb-2 text-sm font-medium text-gray-900 ">Blog</label>
+                <textarea id="body" rows="12" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 value={body}
                 onChange={e => setBody(e.target.value)}
                 ></textarea>
