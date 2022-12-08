d77c75f47cf82c439f92f85770ed772006a70727
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-08 10:47:41 +0300

    Solved the bug in the Delete button

--- src/Components/DeleteModal.jsx
+++ src/Components/DeleteModal.jsx
@@ -35,7 +35,7 @@ export default function DeleteModal({ handleDelete, handleModal, blog }) {
                         <button
                         className="bg-pink-700 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                         type="button"
-                        onClick={handleDelete(blog.id)}
+                        onClick={() => handleDelete(blog.id)}
                         >
                         Yes I'm Sure
                         </button>
