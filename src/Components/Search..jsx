1dabf93cd165b9afa02fdb7ed938985e2befe499
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-10 20:46:13 +0300

    Changed styling to the search bar

--- src/Components/Search..jsx
+++ src/Components/Search..jsx
@@ -19,7 +19,7 @@ const Search = ({ search, setSearch }) => {
                 <input
                     type="text"
                     placeholder="Search"
-                    className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
+                    className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-pink-600"
                     value={search}
                     onChange={e => setSearch(e.target.value)}
                 />
