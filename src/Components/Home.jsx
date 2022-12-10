6a3a6a5e25542d87cdf2ea2eca89379a729239c9
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-10 20:47:39 +0300

    Added loading status and error display functionality

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,14 @@
 import Feed from "./Feed";
 
-const Home = ({blogs}) => { 
+const Home = ({blogs, error, isLoading}) => { 
 
     return (
         <div className="Home flex flex-col space-y-5">
-           {blogs.length ? (<Feed blogs={blogs}/>) : 
-           (<p className="text-center text-3xl text-pink-500 underline py-4 my-10"> The are currently no Blogs available </p>)}
+            {isLoading && <p className="text-3xl my-10">Blogs are loading... </p>}
+            {!isLoading && error && <p className="text-3xl text-red-600 my-10">{error}</p>}
+            {!isLoading && !error && (blogs.length ? <Feed blogs={blogs}/> : 
+                <p className="text-center text-3xl text-pink-500 underline py-4 my-10"> The are currently no Blogs available </p>)
+            }
         </div>
     )
 }
