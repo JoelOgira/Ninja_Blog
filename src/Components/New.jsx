454d7f81ebeac38e8be48601cd872a1ebc0e8761
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-11 13:39:17 +0300

    Refatored the component using context hook

--- src/Components/New.jsx
+++ src/Components/New.jsx
@@ -1,6 +1,34 @@
-const New = ({ 
-    author, setAuthor, title, setTitle, body, setBody, handleSubmit
- }) => {
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { format } from "date-fns";
+import DataContext from "../Context/DataContext";
+import api from "../api/blogs";
+
+
+const New = () => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [body, setBody] = useState('');
+    const { blogs, setBlogs } = useContext(DataContext);
+    const history = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const id = blogs.length ? blogs[blogs.length - 1].id + 1 : 1;
+        const datetime = format(new Date(), 'MMMM yy, yyyy pp');
+        const newBlog = {id, author, datetime, title, body}
+        try {
+            const response = await api.post('/blogs', newBlog);
+            setBlogs([...blogs, response.data]);
+            setTitle('');
+            setAuthor('');
+            setBody('');
+            history('/');
+        } catch (error) {
+            console.log(error.message);
+        }
+    } 
+
     return ( 
         <div className="New">
             <h1 className="text-center text-4xl underline font-semibold mb-3">
@@ -25,7 +53,7 @@ const New = ({
                 </div>
                 <div className="mb-6">                
                     <label htmlFor="body" className="block mb-2 text-sm font-medium text-gray-900 ">Blog</label>
-                    <textarea id="body" rows="12" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    <textarea id="body" rows="16" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     value={body}
                     onChange={e => setBody(e.target.value)}
                     ></textarea>
