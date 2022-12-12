fcfd2813974a5936f25d0f5906802bc37368b089
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-12-12 20:20:09 +0300

    Refactored the code, leaving only states required in the whole application

--- src/Context/DataContext.jsx
+++ src/Context/DataContext.jsx
@@ -1,11 +1,27 @@
 import { createContext, useState, useEffect } from 'react';
+import useAxiosFetch from "../Hooks/useAxiosFetch";
 
 const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
+
+    const [blogs, setBlogs] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
+    const [search, setSearch] = useState('');
+    const { data, error, isLoading } = useAxiosFetch('http://localhost:4200/blogs');
+
+    useEffect(() => {
+        setBlogs(data);
+    }, [data])
+
+    useEffect(() => {
+        const filteredResults = blogs.filter((blog) => ((blog.title).toLowerCase().includes(search.toLowerCase()) || ((blog.author).toLowerCase().includes(search.toLowerCase()))));
+        setSearchResults(filteredResults.reverse());
+    }, [blogs, search])   
+
     return (
         <DataContext.Provider value={{
-
+            search, setSearch, searchResults, error, isLoading, blogs, setBlogs
         }}>
             {children}
         </DataContext.Provider>
